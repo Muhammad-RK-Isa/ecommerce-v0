2b603a8c05eaad88806c95fa449953db438ca2e2
muhammad-rk-isa <muhammad.rk.isa@gmail.com>
2025-04-19 23:05:11 +0600

    style: format using biome

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,36 +4,36 @@ import { cn } from "~/lib/utils";
 import "~/styles/globals.css";
 
 const fontSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
 });
 
 const fontMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Ecommerce v0",
-  description: "A minimal ecommerce application",
+	title: "Ecommerce v0",
+	description: "A minimal ecommerce application",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          fontSans.variable,
-          fontMono.variable,
-          "antialiased scroll-smooth"
-        )}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body
+				className={cn(
+					fontSans.variable,
+					fontMono.variable,
+					"antialiased scroll-smooth",
+				)}
+			>
+				{children}
+			</body>
+		</html>
+	);
 }
